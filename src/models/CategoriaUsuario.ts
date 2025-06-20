aced133624e38ea1fc87b6e1a2c83bd0a8d0ab88
Byanca <byancamay1@gmail.com>
2025-06-20 18:45:38 -0300

    commit pra teste

---
+++ src/models/CategoriaUsuario.ts
@@ -0,0 +1,11 @@
+export class CategoriaUsuario { //teste
+
+    id: number;
+    nome: string;
+
+    constructor(id: number, nome: string){ //comentário para teste
+        this.id = id;
+        this.nome = nome; //nome
+
+    }
+}
